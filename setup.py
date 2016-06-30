29dd27973111abbec2c05c0c51e62c65347f6206
Thor77 <xXThor77Xx@gmail.com>
2016-06-30 23:00:51 +0200

    dump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MarkovPy',
-    version='0.1.0',
+    version='0.2.0',
     description='Markov-implementation with redis-database',
     author='Thor77',
     author_email='thor77@thor77.org',
