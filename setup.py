eb7f4a430341f73af1bbcede91b8bb1ac2a01b38
Thor77 <xXThor77Xx@gmail.com>
2016-07-17 19:59:52 +0200

    bump version to 0.5.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MarkovPy',
-    version='0.5.2',
+    version='0.5.3',
     description='A simple Markovchain-Implementation',
     author='Thor77',
     author_email='thor77@thor77.org',
