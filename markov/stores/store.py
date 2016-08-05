9b1dd3ad8cf14ed177417dfaadb4e86591c7e6de
Thor77 <thor77@thor77.org>
2016-08-05 16:14:10 +0200

    Add .insert_previous and .previous_words to blueprint-store (markov.stores.Store)

--- markov/stores/store.py
+++ markov/stores/store.py
@@ -8,7 +8,13 @@ class Store:
 
     def insert(self, word, next_word):
         '''
-        Create a relation between `word` and `next_word`
+        Create a relation between `word` and `next_word` (forward)
+        '''
+        pass
+
+    def insert_previous(self, word, previous_word):
+        '''
+        Create a relation between `word` and `previous_word` (backward)
         '''
         pass
 
@@ -26,13 +32,25 @@ class Store:
 
     def next_words(self, word):
         '''
-        Return all words related to `word`
+        Return all words related to `word` (forward)
 
         :param word: target word
         :type word: str
 
-        :return: words related to `word`
+        :return: words related to `word` (forward)
         :rtype [markov.markov.Word]
+        '''
+        return [Word('', score=0)]
+
+    def previous_words(self, word):
+        '''
+        Return all words related to `word` (backward)
+
+        :param word: target word
+        :type word: str
+
+        :return: words related to `word` (backward)
+        :rtype: [markov.markov.Word]
         '''
         return [Word('', score=0)]
 
